a31c399a32288c2a1b7b2d80aefa6f30fea1541a
ilan szenker <szenkerilan@gmail.com>
2019-08-02 10:10:41 +0300

    adding break

--- js/request-logic.js
+++ js/request-logic.js
@@ -10,13 +10,18 @@ function getContentType(filename) {
   switch(extension){
     case "html":
       return "text/html";
+      break;
     case "css":
       return "text/css";
+      break;
     case "jpeg":
       return "image/jpeg";
+      break;
     case "jpg":
       return "image/jpeg";
+      break;
     default:
       return "text/plain" ;
+      break;
   }
 }
